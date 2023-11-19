5ce69bfed0f8e9af6e9449f0624bbb08beb587c6
Abdillahi485 <daakircadde143@gmail.com>
2023-11-19 16:16:30 +0300

    solved

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function getFirstNames(firstNames){
+    for(let i=0; i<firstNames.length;i++){
+        for(j=0;j<firstNames[i].length;j++){
+            console.log(firstNames[i][0])
+        }
+    }
+    }
+    
+    
+    let firstNames= [["Ahmed","Aisha","Amina"],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]]
+    getFirstNames(firstNames)
+    
